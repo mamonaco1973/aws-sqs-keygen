0027fb1c64d78e9357857f3b39692b4c113723a3
Mike Monaco <mamonaco1973@gmail.com>
2025-11-07 11:27:06 -0500

    add dynamo table to the mix

--- 03-lambdas/lambda-get.tf
+++ 03-lambdas/lambda-get.tf
@@ -1,8 +1,8 @@
 # ================================================================================================
-# Lambda: Requester  (enqueue job)
+# Lambda: Responder  (retrieve job result)
 # ================================================================================================
-resource "aws_iam_role" "lambda_requester_role" {
-  name = "lambda-requester-role"
+resource "aws_iam_role" "lambda_get_role" {
+  name = "lambda-get-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,42 +14,54 @@ resource "aws_iam_role" "lambda_requester_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_requester_basic" {
-  role       = aws_iam_role.lambda_requester_role.name
+# --------------------------------------------------------------------------------
+# Attach basic CloudWatch Logs policy
+# --------------------------------------------------------------------------------
+resource "aws_iam_role_policy_attachment" "lambda_get_basic" {
+  role       = aws_iam_role.lambda_get_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy" "lambda_requester_sqs" {
-  name = "lambda-requester-sqs"
-  role = aws_iam_role.lambda_requester_role.id
+# --------------------------------------------------------------------------------
+# Inline Policy: Allow Lambda to read from DynamoDB table
+# --------------------------------------------------------------------------------
+resource "aws_iam_role_policy" "lambda_get_dynamo" {
+  name = "lambda-get-dynamo"
+  role = aws_iam_role.lambda_get_role.id
 
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [{
-      Effect   = "Allow"
-      Action   = ["sqs:SendMessage"]
-      Resource = data.aws_sqs_queue.keygen_input.arn
+      Effect   = "Allow",
+      Action   = ["dynamodb:GetItem"],
+      Resource = aws_dynamodb_table.keygen_results.arn
     }]
   })
 }
 
-resource "aws_lambda_function" "lambda_requester" {
-  function_name = "keygen-requester"
-  role          = aws_iam_role.lambda_requester_role.arn
+# --------------------------------------------------------------------------------
+# Lambda Function Definition
+# --------------------------------------------------------------------------------
+resource "aws_lambda_function" "lambda_get" {
+  function_name = "keygen-get"
+  role          = aws_iam_role.lambda_get_role.arn
   runtime       = "python3.11"
-  handler       = "post.lambda_handler"
-  filename      = data.archive_file.requester_zip.output_path
+  handler       = "get.lambda_handler"
+  filename      = data.archive_file.lambda_get_zip.output_path
   timeout       = 15
 
   environment {
     variables = {
-      REQ_QUEUE_URL = data.aws_sqs_queue.keygen_input.url
+      RESULTS_TABLE = aws_dynamodb_table.keygen_results.name
     }
   }
 }
 
-data "archive_file" "requester_zip" {
+# --------------------------------------------------------------------------------
+# Archive Lambda Code (ZIP packaging)
+# --------------------------------------------------------------------------------
+data "archive_file" "lambda_get_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/code/post"
-  output_path = "${path.module}/code/post.zip"
+  source_dir  = "${path.module}/code/get"
+  output_path = "${path.module}/code/get.zip"
 }
