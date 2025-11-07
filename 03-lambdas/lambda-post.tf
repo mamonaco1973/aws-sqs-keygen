7b836e059f75ff083967585a192d56bf5937980d
Mike Monaco <mamonaco1973@gmail.com>
2025-11-07 14:06:20 -0500

    consolidate lambda into one zip

--- 03-lambdas/lambda-post.tf
+++ 03-lambdas/lambda-post.tf
@@ -1,8 +1,8 @@
 # ================================================================================================
-# Lambda: Requester  (enqueue job)
+# Lambdaa Function: Keygen Post (SQS Requester)
 # ================================================================================================
-resource "aws_iam_role" "lambda_requester_role" {
-  name = "lambda-requester-role"
+resource "aws_iam_role" "lambda_post_role" {
+  name = "lambda-post-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -14,14 +14,14 @@ resource "aws_iam_role" "lambda_requester_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_requester_basic" {
-  role       = aws_iam_role.lambda_requester_role.name
+resource "aws_iam_role_policy_attachment" "lambda_post_basic" {
+  role       = aws_iam_role.lambda_post_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_role_policy" "lambda_requester_sqs" {
+resource "aws_iam_role_policy" "lambda_post_sqs" {
   name = "lambda-requester-sqs"
-  role = aws_iam_role.lambda_requester_role.id
+  role = aws_iam_role.lambda_post_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -33,9 +33,9 @@ resource "aws_iam_role_policy" "lambda_requester_sqs" {
   })
 }
 
-resource "aws_lambda_function" "lambda_requester" {
-  function_name = "keygen-requester"
-  role          = aws_iam_role.lambda_requester_role.arn
+resource "aws_lambda_function" "lambda_post" {
+  function_name = "keygen-post"
+  role          = aws_iam_role.lambda_post_role.arn
   runtime       = "python3.11"
   handler       = "post.lambda_handler"
   filename      = data.archive_file.lambdas_zip.output_path
